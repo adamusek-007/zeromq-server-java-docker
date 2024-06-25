11a3d0515afb4e80803bff97ad30554a8ea6b00c
adamusek-007 <adam.kuczabinski@gmail.com>
2024-06-25 17:08:59 +0200

    Add 0mq to app.java

--- pico-server/src/main/java/pl/bic/App.java
+++ pico-server/src/main/java/pl/bic/App.java
@@ -1,13 +1,27 @@
 package pl.bic;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try (ZContext context = new ZContext()) {
+            Socket socket = context.createSocket(SocketType.PULL);
+            socket.bind("tcp://*:5555");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                String message = socket.recvStr();
+                System.out.println("Received message: " + message);
+
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
